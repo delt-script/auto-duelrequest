d1a781c9ff54eeb8434662241da775cc25e1c906
delt-script <github.123abc@gmail.com>
2026-04-22 17:58:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- [[ 16進数ゲリラ通信：Delta側 ]]
 local data = "日本語テスト：成功への一歩" 
-local url = "お前のGASのURL"
+local url = "https://script.google.com/macros/s/AKfycby_TzKrZNOz1gvJ-gy4x4pn509wZcICHHfyAazXT9htKEFhmrcxolVZxZbt4o_sYJE7/exec"
 
 -- 文字列を16進数に変換する関数
 local function toHex(str)
